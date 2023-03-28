412ca81394d702b8784d5df75f0bf941470ee359
corno <corno.astn.a@schraverus.com>
2023-03-28 10:17:56 +0200

    .

--- pareto/src/data/glossary.data.ts
+++ pareto/src/data/glossary.data.ts
@@ -28,31 +28,34 @@ const d = pd.d
 export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
     'parameters': d({}),
     'imports': d({}),
-    'types': d({
-        "Boolean": type(boolean()),
-        "Null": type(null_()),
-        "Dictionary": parametrizedType({ "Type": null }, dictionary(ref(typeParameter("Type")))),
-        "Number": type(number()),
-        "Path": type(nested(string())),
-        "String": type(string()),
-        "AnnotatedDictionary": parametrizedType({ "Annotation": null, "Type": null }, group({
-            "annotation": member(ref(typeParameter("Annotation"))),
-            "dictionary": member(dictionary(ref(typeParameter("Type")))),
-        })),
-        "AnnotatedKey": parametrizedType({ "Annotation": null }, group({
-            "annotation": member(ref(typeParameter("Annotation"))),
-            "key": member(string()),
-        })),
-        "AnnotatedReference": parametrizedType({ "Annotation": null, "ReferencedType": null }, group({
-            "annotation": member(ref(typeParameter("Annotation"))),
-            "referencedValue": member(ref(typeParameter("ReferencedType"))),
-        })),
-        "AnnotatedKeyReferencePair": parametrizedType({ "Annotation": null, "ReferencedType": null }, group({
-            "annotation": member(ref(typeParameter("Annotation"))),
-            "key": member(string()),
-            "referencedValue": member(ref(typeParameter("ReferencedType"))),
-        })),
-    }),
+    'root': {
+        'namespaces': d({}),
+        'types': d({
+            "Boolean": type(boolean()),
+            "Null": type(null_()),
+            "Dictionary": parametrizedType({ "Type": null }, dictionary(ref(typeParameter("Type")))),
+            "Number": type(number()),
+            "Path": type(nested(string())),
+            "String": type(string()),
+            "AnnotatedDictionary": parametrizedType({ "Annotation": null, "Type": null }, group({
+                "annotation": member(ref(typeParameter("Annotation"))),
+                "dictionary": member(dictionary(ref(typeParameter("Type")))),
+            })),
+            "AnnotatedKey": parametrizedType({ "Annotation": null }, group({
+                "annotation": member(ref(typeParameter("Annotation"))),
+                "key": member(string()),
+            })),
+            "AnnotatedReference": parametrizedType({ "Annotation": null, "ReferencedType": null }, group({
+                "annotation": member(ref(typeParameter("Annotation"))),
+                "referencedValue": member(ref(typeParameter("ReferencedType"))),
+            })),
+            "AnnotatedKeyReferencePair": parametrizedType({ "Annotation": null, "ReferencedType": null }, group({
+                "annotation": member(ref(typeParameter("Annotation"))),
+                "key": member(string()),
+                "referencedValue": member(ref(typeParameter("ReferencedType"))),
+            })),
+        }),
+    },
     'asynchronous': {
         'interfaces': d({
             "String": aInterface(aInterfaceMethod(typeReference("String"))),
